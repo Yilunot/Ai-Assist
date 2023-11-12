8a1ddd5bfbb1ad13489bb32b11aedd6fd8d34499
Tony Liu <80366426+Yilunot@users.noreply.github.com>
2023-11-12 03:11:40 -0800

    AI
    AI assistant

---
+++ AI.py
@@ -0,0 +1,124 @@
+#python program to translate
+#speech to text and text to speech
+#python 3.11.6
+#pip install SpeechRecognition
+#pip install pyttsx3
+
+import speech_recognition as sr
+import pyttsx3
+import time
+import os
+import subprocess
+import webbrowser
+from dotenv import load_dotenv
+load_dotenv()
+import openai
+OPENAI_KEY = os.getenv('OPENAI_KEY')
+openai.api_key = OPENAI_KEY
+
+
+
+if OPENAI_KEY is None:
+    raise ValueError("OPENAI_KEY environment variable is not set.")
+
+# Python program to translate
+# speech to text and text to speech
+
+# Initialize the recognizer 
+r = sr.Recognizer() 
+
+# Function to convert text to
+# speech
+def SpeakText(command):
+	
+	# Initialize the engine
+	engine = pyttsx3.init()
+	engine.say(command) 
+	engine.runAndWait()
+
+# File to store transcriptions
+transcription_file = "transcriptions.txt"
+
+def save_transcription(role,text):
+    with open(transcription_file, "a") as file:
+        file.write(f"{role.capitalize()}: {text}\n")
+	
+# Loop infinitely for user to
+# speak
+def record_text():
+    while True:
+        try:
+            with sr.Microphone() as source2:
+                r.adjust_for_ambient_noise(source2, duration=0.2)
+                print("Say Hey Ian")
+                audio2 = r.listen(source2, timeout=5)
+                wake_word = r.recognize_google(audio2).lower()
+                
+
+                if "hey ian" in wake_word:
+                    print("IAN activated. Please speak...")
+                    audio2 = r.listen(source2)
+                    MyText = r.recognize_google(audio2)
+                    return MyText
+
+        except sr.RequestError as e:
+            print(f"Could not request results; {e}")
+        except sr.UnknownValueError:
+            print("Waiting for your command")
+
+def open_application(application_name):
+    try:
+        if "notepad" in application_name.lower():
+            subprocess.Popen(["notepad.exe"])
+        if "spotify" in application_name.lower():
+            subprocess.Popen(["C:\\Users\\Yilunot\\AppData\\Roaming\\Spotify\\Spotify.exe"])
+        elif "browser" in application_name.lower():
+           webbrowser.open("https://www.google.com")
+        elif "youtube" in application_name.lower():
+           webbrowser.open("https://www.youtube.com")
+
+        else:
+            print(f"Unknown application: {application_name}")
+    except Exception as e:
+        print(f"Error opening {application_name}: {e}")
+
+
+def send_to_chatGPT(messages, model= "gpt-3.5-turbo"):
+    response = openai.ChatCompletion.create(
+        model= model,
+        messages = messages,
+        max_tokens =100,
+        n=1,
+        stop=None,
+        temperature = 0.5,
+      )
+    message = response.choices[0].message.content
+    messages.append(response.choices[0].message)
+    return message
+   
+   
+messages = [{"role": "user","content": " be a IA Assistant."}]
+
+while True:
+  text =record_text()
+  messages.append({"role": "user","content": text})
+  print(f"\x1b[31mMe:{text}\x1b[0m")
+  save_transcription("Me:",text)
+
+  if "open Notepad" in text:
+    open_application("notepad")
+  elif "open Spotify"in text:
+      open_application("spotify")
+  elif "open browser" in text:
+    open_application("browser")
+  elif "open YouTube" in text:
+     open_application("youtube")
+  elif "bye" in text:
+    print(f"\x1b[32mAssistant:Good Bye\x1b[0m")
+    break;
+  else:
+    print(f"\x1b[32mAssistant: Thinking...\x1b[0m")
+    response = send_to_chatGPT(messages)
+    print(f"\x1b[32mAssistant:{response}\x1b[0m")
+    SpeakText(response)
+    save_transcription("AI:",response)
